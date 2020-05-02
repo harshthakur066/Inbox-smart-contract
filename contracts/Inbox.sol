3da1f9440b9fe5c2395f651a1066e77d4858e762
harshthakur066 <harshthakur066@gmail.com>
2020-05-02 23:22:30 +0530

    compile.js and constructor error fixed

--- contracts/Inbox.sol
+++ contracts/Inbox.sol
@@ -1,15 +1,16 @@
 pragma solidity ^0.4.17;
 
+
+// No need of any function to get or read the values for variables, if they are public it will be automatically formed
+
 contract Inbox {
     string public message;
-    // No need of any function to get or read the values for variables, if they are public it will be automatically formed
-    
-    Inbox(string initialMessage) public {
+
+    function Inbox(string initialMessage) public {
         message = initialMessage;
     }
-    
+
     function setMessage(string newMessage) public {
         message = newMessage;
     }
-    
 }
